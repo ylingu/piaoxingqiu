ecb01e387d9292a9c443c9169aa36c1c93e42851
lmsh7 <36391487+lmsh7@users.noreply.github.com>
2023-09-27 03:17:17 +0000

    添加选座

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
-# 输入自己的token
-token = 'your token'
+# 输入自己的token, 通过cyy_gatewayapi/trade/buyer/order查看
+token =''
 # 项目id，必填
-show_id = '644fcb2aca916100017dcfef'
+show_id = '6443a0d135bfd50001a7c7db'
 # 指定场次id，不指定则默认从第一场开始遍历
 session_id = ''  # 644fcb7dca916100017dda3d
 # 购票数量，一定要看购票须知，不要超过上限
-buy_count = 2
+buy_count = 1
 # 指定观演人，观演人序号从0开始，人数需与票数保持一致
-audience_idx = [0, 1]
+audience_idx = [1]
 # 门票类型，不确定则可以不填，让系统自行判断。快递送票:EXPRESS,电子票:E_TICKET,现场取票:VENUE,电子票或现场取票:VENUE_E,目前只发现这四种，如有新发现可补充
 deliver_method = ''
+seat_list = ['四楼427区', '二楼203区']
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import request
 import config
 
-'''
+"""
 目前仅支持【无需选座】的项目
-'''
+"""
 show_id = config.show_id
 session_id = config.session_id
 buy_count = config.buy_count
 audience_idx = config.audience_idx
 deliver_method = config.deliver_method
-seat_plan_id = ''
+seat_list = config.seat_list
+seat_plan_id = ""
 session_id_exclude = []  # 被排除掉的场次
 price = 0
 
@@ -22,7 +23,10 @@ while True:
                 sessions = request.get_sessions(show_id)
                 if sessions:
                     for i in sessions:
-                        if i["sessionStatus"] == 'ON_SALE' and i["bizShowSessionId"] not in session_id_exclude:
+                        if (
+                            i["sessionStatus"] == "ON_SALE"
+                            and i["bizShowSessionId"] not in session_id_exclude
+                        ):
                             session_id = i["bizShowSessionId"]
                             print("session_id:" + session_id)
                             break
@@ -34,30 +38,51 @@ while True:
         # 获取座位余票信息，默认从最低价开始
         seat_plans = request.get_seat_plans(show_id, session_id)
         seat_count = request.get_seat_count(show_id, session_id)
-        print(seat_count)
+        mp = {
+            i["seatPlanId"]: (None, None)
+            for i in seat_count
+            if i["canBuyCount"] >= buy_count
+        }
+        for i in seat_plans:
+            if i["seatPlanId"] in mp:
+                mp[i["seatPlanId"]] = (i["seatPlanName"], i["originalPrice"])
 
-        for i in seat_count:
-            if i["canBuyCount"] >= buy_count:
-                seat_plan_id = i["seatPlanId"]
-                for j in seat_plans:
-                    if j["seatPlanId"] == seat_plan_id:
-                        price = j["originalPrice"]  # 门票单价
-                        break
+        # 符合i["seatPlanName"] in seat_list中最便宜的座位
+        for k, (n, p) in mp.items():
+            if n in seat_list:
+                seat_plan_id = k
+                price = p
                 break
+
         # 如果没有拿到seat_plan_id，说明该场次所有座位的余票都不满足购票数量需求，就重新开始刷下一场次
         if not seat_plan_id:
             print("该场次" + session_id + "没有符合条件的座位，将为你继续搜寻其他在售场次")
             session_id_exclude.append(session_id)  # 排除掉这个场次
-            session_id = ''
+            session_id = ""
             continue
 
         if not deliver_method:
-            deliver_method = request.get_deliver_method(show_id, session_id, seat_plan_id, price, buy_count)
+            deliver_method = request.get_deliver_method(
+                show_id, session_id, seat_plan_id, price, buy_count
+            )
         print("deliver_method:" + deliver_method)
 
         if deliver_method == "VENUE_E":
-            request.create_order(show_id, session_id, seat_plan_id, price, buy_count, deliver_method, 0, None,
-                                 None, None, None, None, [])
+            request.create_order(
+                show_id,
+                session_id,
+                seat_plan_id,
+                price,
+                buy_count,
+                deliver_method,
+                0,
+                None,
+                None,
+                None,
+                None,
+                None,
+                [],
+            )
         else:
             # 获取观演人信息
             audiences = request.get_audiences()
@@ -75,21 +100,51 @@ while True:
                 detail_address = address["detailAddress"]  # 详细地址
 
                 # 获取快递费用
-                express_fee = request.get_express_fee(show_id, session_id, seat_plan_id, price, buy_count,
-                                                      location_city_id)
+                express_fee = request.get_express_fee(
+                    show_id,
+                    session_id,
+                    seat_plan_id,
+                    price,
+                    buy_count,
+                    location_city_id,
+                )
 
                 # 下单
-                request.create_order(show_id, session_id, seat_plan_id, price, buy_count, deliver_method,
-                                     express_fee["priceItemVal"], receiver,
-                                     cellphone, address_id, detail_address, location_city_id, audience_ids)
+                request.create_order(
+                    show_id,
+                    session_id,
+                    seat_plan_id,
+                    price,
+                    buy_count,
+                    deliver_method,
+                    express_fee["priceItemVal"],
+                    receiver,
+                    cellphone,
+                    address_id,
+                    detail_address,
+                    location_city_id,
+                    audience_ids,
+                )
             elif deliver_method == "VENUE" or deliver_method == "E_TICKET":
-                request.create_order(show_id, session_id, seat_plan_id, price, buy_count, deliver_method, 0, None,
-                                     None, None, None, None, audience_ids)
+                request.create_order(
+                    show_id,
+                    session_id,
+                    seat_plan_id,
+                    price,
+                    buy_count,
+                    deliver_method,
+                    0,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    audience_ids,
+                )
             else:
                 print("不支持的deliver_method:" + deliver_method)
         break
     except Exception as e:
         print(e)
         session_id_exclude.append(session_id)  # 排除掉这个场次
-        session_id = ''
-
+        session_id = ""
